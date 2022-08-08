7802d7b4a8b7a3ae38df6c5852ec84ed5e5b8b5a
xTZyyds <270040328@yoobeestudent.ac.nz>
2022-08-08 13:01:02 +1200

    Updated requirements.txt

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'RDS_DB_NAME' in os.environ:
+    print("what's HAPPENING HERE??")
+    print(os.environ['RDS_DB_NAME'])
+    print(os.environ['RDS_USERNAME'])
+    print(os.environ['RDS_USERNAME'])
+    print(os.environ['RDS_HOSTNAME'])
+    print(os.environ['RDS_PORT'])
+    print("end of prints")
     DATABASES = {
             'default': {
                 'ENGINE':'django.db.backends.postgresql_psycopg2',
                 'NAME': os.environ['RDS_DB_NAME'],
                 'USER': os.environ['RDS_USERNAME'],
-                'PASSWORD': os.environ['RDS_PASSWORD'],
+                'PASSWORD': os.environ['RDS_USERNAME'],
                 'HOST': os.environ['RDS_HOSTNAME'],
                 'PORT': os.environ['RDS_PORT'],
             }
